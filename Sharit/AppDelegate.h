8b723f26a1a9f10237e74f92b3ae46abe799e991
Eugene Dorfman <justadreamer2007@gmail.com>
2012-05-10 20:18:11 +0300

    Initial Commit

---
+++ Sharit/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Sharit
+//
+//  Created by Eugene Dorfman on 5/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Sharit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sharit
+//
+//  Created by Eugene Dorfman on 5/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
