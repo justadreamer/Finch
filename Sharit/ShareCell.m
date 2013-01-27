382d9f78acb354854cdb5903fcc2c1520c052dbf
Eugene Dorfman <eugene@Eugenes-MacBook-Pro.local>
2013-01-27 16:30:08 +0200

    improved styling of ShareCell

--- Sharit/ShareCell.m
+++ Sharit/ShareCell.m
@@ -19,13 +19,6 @@
     self.check.hidden = ![adapter showCheckMark];
     self.titleLabel.text = [adapter mainText];
     self.detailLabel.text = [adapter detailText];
-    if (0==[self.detailLabel.text length]) {
-        CGSize size = self.titleLabel.superview.bounds.size;
-        CGPoint center = CGPointMake(size.width/2, size.height/2);
-        self.titleLabel.center = CGPointMake(self.titleLabel.center.x, center.y);
-    } else {
-        self.titleLabel.center = CGPointMake(self.titleLabel.center.x, self.detailLabel.center.y-self.titleLabel.frame.size.height);
-    }
     self.detailLabel.textColor = [adapter detailTextColor];
 }
 
